27f5b7d7f82f035809f9f469d6195898f6d04b61
Fwangshak Mishwatts <itsmishesbone@gmail.com>
2021-12-14 17:34:21 -0800

    new updates to test units

---
+++ PacmanGameLogicEntityUnityTest/DirectionTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PacmanGameLogic
+{
+    [TestClass]
+    //creates a test class
+    public class DirectionTest
+    {
+        [TestMethod]
+        //creates a test method
+        public void TestingGhostMove()
+        {
+            //Actual functionality
+            DirectionTest directionTest = new DirectionTest();
+            //expected functionality
+            directionTest.TestingGhostMove();
+            //Asserts Fuctionality
+            Assert.IsNotNull(directionTest);    
+
+        }
+    }
+}
---
+++ PacmanGameLogicEntityUnityTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PacmanGameLogicEntityUnityTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PacmanGameLogicEntityUnityTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e14fe35e-a306-4914-9dab-a1487366dd61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
